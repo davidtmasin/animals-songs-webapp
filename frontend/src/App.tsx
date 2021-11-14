ffaf4cff7e2055404d91d0a374194658015d6acd
davidtmasin <davidteixeira.info@gmail.com>
2021-11-14 10:42:06 -0300

    Project Clean

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//Este import tornou-se opcional desde a versão 17 do React, sendo assim, ele pode ser removido do código
+//import React from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Sons dos Animais</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
